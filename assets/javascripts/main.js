bd5424ca913fb00b3ab21e4e34e6198c451a5710
maxhall <maxwelljhall@gmail.com>
2016-09-18 16:03:27 +1000

    President Summary update sequence
    - Assorted data playing

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -2,11 +2,15 @@
 var pymChild = new pym.Child();
 
 /**
- * Grab the data from our Google Sheet
+ * Grab the data from our Google Sheet and clean it up a little
  */
 var sheetURL = 'https://docs.google.com/spreadsheets/d/1YrPVj8O8ZrZaByz8wMhS56N8rL7e2hK-in_cyWlrq4I/pubhtml';
-var rawSheetData = {};
+
+var rawData = {};
 var constants = {};
+var updatesData = {};
+var presData = {};
+var honiData = {};
 
 var tabletopInit = function tabletopInit() {
   Tabletop.init({
@@ -17,40 +21,51 @@ var tabletopInit = function tabletopInit() {
 };
 
 var logTabletopData = function logTabletopData(data, tabletop) {
-  rawSheetData = data;
-  constants = rawSheetData.constants.elements['0'];
+  rawData = data;
+  constants = data.constants.elements['0'];
+  updatesData = data.updates.elements;
+  presData = data.pres.elements;
+  honiData = data.honi.elements;
   d3.select('.header__standfirst').html(constants.standfirst);
 };
 
 /**
  * President
  */
+
 var presWinningPercentage = 48;
 var presSummaryHeight = 40;
 var presSummaryWidth = $('.js-pres-summary').width();
 
-// Set up the pres summary
+// Elements of the summary graphic
+var presSummary = d3.select('.js-pres-summary').append('svg');
+var presSummarySegmentOne = presSummary.append('rect');
+var presSummarySegmentTwo = presSummary.append('rect');
+var presSummaryMidline = presSummary.append('line');
+var presSummaryLabelOne = presSummary.append('text');
+var presSummaryLabelTwo = presSummary.append('text');
+
+// Initiate the pres summary
 var presSummaryInit = function presSummaryInit() {
-  var presSummary = d3.select('.js-pres-summary')
-    .append('svg')
+  presSummary
     .attr('height', presSummaryHeight)
     .attr('width', presSummaryWidth);
 
-  var presSummarySegmentOne = presSummary.append('rect')
+  presSummarySegmentOne
     .attr('x', 0)
     .attr('y', 0)
     .attr('width', presSummaryWidth * presWinningPercentage / 100)
     .attr('height', presSummaryHeight)
     .style('fill', '#008938');
 
-  var presSummarySegmentTwo = presSummary.append('rect')
+  presSummarySegmentTwo
     .attr('x', presSummaryWidth * (presWinningPercentage / 100))
     .attr('y', 0)
     .attr('width', presSummaryWidth * (100 - presWinningPercentage) / 100)
     .attr('height', presSummaryHeight)
     .style('fill', '#753da6');
 
-  var presSummaryMidline = presSummary.append('line')
+  presSummaryMidline
     .attr('x1', presSummaryWidth/2)
     .attr('y1', 0)
     .attr('x2', presSummaryWidth/2)
@@ -58,14 +73,14 @@ var presSummaryInit = function presSummaryInit() {
     .attr('stroke-width', 2)
     .attr('stroke', 'black');
 
-  var segmentLabel = presSummary.append('text')
+  presSummaryLabelOne
     .attr('x', presSummaryHeight/4)
     .attr('y', presSummaryHeight/4)
     .attr('dy', presSummaryHeight/2)
     .classed('pres__summary-segment-label', true)
     .text(presWinningPercentage + '%');
 
-  var segmentLabelTwo = presSummary.append('text')
+  presSummaryLabelTwo
     .attr('x', presSummaryWidth - 70)
     .attr('y', presSummaryHeight/4)
     .attr('dy', presSummaryHeight/2)
@@ -73,10 +88,37 @@ var presSummaryInit = function presSummaryInit() {
     .text(100 - presWinningPercentage + '%');
 };
 
+// Update the pres summary after resize or new data
 var presSummaryUpdate = function presSummaryUpdate() {
   console.log('Updating pres summary');
+
+  presWinningPercentage = Math.random() * 100 | 0;
+  var presSummaryWidth = $('.js-pres-summary').width();
+
+  presSummarySegmentOne
+    .transition()
+    .attr('width', presSummaryWidth * presWinningPercentage / 100);
+
+  presSummarySegmentTwo
+    .transition()
+    .attr('x', presSummaryWidth * (presWinningPercentage / 100))
+    .attr('width', presSummaryWidth * (100 - presWinningPercentage) / 100);
+
+  presSummaryMidline
+    .attr('x1', presSummaryWidth/2)
+    .attr('x2', presSummaryWidth/2);
+
+  presSummaryLabelOne
+    .text(presWinningPercentage + '%');
+
+  presSummaryLabelTwo
+    .attr('x', presSummaryWidth - 70)
+    .text(100 - presWinningPercentage + '%');
 };
 
+/**
+ * Set up the user interface
+ */
 var tabsInit = function tabsInit() {
   $('ul.tabs li').click(function(){
     var tab_id = $(this).attr('data-tab');
@@ -98,10 +140,21 @@ var sirenInit = function sirenInit() {
   })
 }
 
-// Initialise everything
-$(document).ready(function(){
+/**
+ * Initialise everything
+ */
+
+var init = function init() {
   tabsInit();
   sirenInit();
   presSummaryInit();
-  // tabletopInit();
+  tabletopInit();
+
+  $(window).resize(function() {
+    presSummaryUpdate();
+  })
+};
+
+$(document).ready(function(){
+  init();
 })
